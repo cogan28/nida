25ece5ae700c5dce1477374d76a7574cc9bf5af1
cogan28 <77156794+cogan28@users.noreply.github.com>
2021-01-23 01:41:23 +0700

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,166 @@
+const help = (prefix) => { 
+	return `              
+  *Instagram Follow*
+ instagram.com/_sekhaa
+ 
+_*>FlinSky_BOT<*_
+
+ Hi, _*undefined*_ 
+ Berikut adalah fitur pada bot!
+_*Fun*_
+├ *${prefix}truth*
+├ *${prefix}dare*
+├ *${prefix}quotes*
+├ *${prefix}hilih*
+├ *${prefix}gombalan*
+├ *${prefix}persengay*
+├ *${prefix}alay* [text]
+├ *${prefix}simi* [text]
+├ *${prefix}bucin*
+├ *${prefix}gtts* [text]
+└ *${prefix}tts*
+
+_*Kerang*_
+├ *${prefix}apakah*
+├ *${prefix}bisakah*
+├ *${prefix}kapankah*
+└ *${prefix}rate*
+
+_*Wibu*_
+├ *${prefix}loli*
+├ *${prefix}kiss*
+├ *${prefix}peluk*
+├ *${prefix}anime*
+├ *${prefix}randomcry*
+├ *${prefix}randomanime*
+├ *${prefix}waifu*
+├ *${prefix}waifu2*
+├ *${prefix}nakonime*
+├ *${prefix}nekonime2*
+├ *${prefix}wibu*
+├ *${prefix}wait*
+└ *${prefix}pokemon*
+
+
+_*Nsfw Menu*_
+├ *${prefix}hentai*
+├ *${prefix}randomhentai*
+├ *${prefix}nsfwblowjob*
+├ *${prefix}nsfwtrap*
+└ *${prefix}nsfwneko*
+
+_*Sticker*_
+├ *${prefix}sticker*
+├ *${prefix}stiker*
+├ *${prefix}stickergif*
+├ *${prefix}sgif*
+├ *${prefix}s*
+├ *${prefix}toimg*
+├ *${prefix}tstiker*
+└ *${prefix}tsticker*
+
+_*Search*_
+├ *${prefix}beritahoax*
+├ *${prefix}ytsearch* [yt search]
+├ *${prefix}lirik* [judul lagu]
+├ *${prefix}chord* [judul lagu]
+├ *${prefix}wikien* [love]
+├ *${prefix}url2img* [link]
+├ *${prefix}shorturl*
+├ *${prefix}ssweb* [link]
+├ *${prefix}fototiktok* [username]
+├ *${prefix}map* [kota]
+├ *${prefix}indohot* 
+├ *${prefix}kbbi* [kamus]
+└ *${prefix}infoghitub* 
+
+
+_*Group*_
+├ *${prefix}mentionall*
+├ *${prefix}mentionall2*
+├ *${prefix}tagall*
+├ *${prefix}setdesc*
+├ *${prefix}setname*
+├ *${prefix}kick* [tag]
+├ *${prefix}add* [628xxx]
+├ *${prefix}promote* [tag]
+├ *${prefix}demote* [tag]
+├ *${prefix}group* [buka]
+├ *${prefix}group* [tutup]
+├ *${prefix}linkgc*
+├ *${prefix}setpp* [foto kau]
+├ *${prefix}groupinfo*
+├ *${prefix}tagme*
+├ *${prefix}nsfw* [1/0]
+├ *${prefix}openanime* [1/0]
+├ *${prefix}simih* [1/0]
+├ *${prefix}welcome* [1/0]
+├ *${prefix}edotensei*
+├ *${prefix}listadmins*
+└ *${prefix}speed*
+
+_*Textpro*_
+├ *${prefix}ninjalogo* [text|text]
+├ *${prefix}marvelogo* [text|text]
+├ *${prefix}snowrite* [text|text]
+├ *${prefix}water* [text]
+├ *${prefix}text3d* [text]
+├ *${prefix}ninjalogo* [text|text]
+├ *${prefix}wolflogo* [text|text]
+└ *${prefix}lionlogo* [text|text]
+
+_*Primbon*_
+├ *${prefix}ramaljadian* [nama|nama]
+├ *${prefix}primbonjodoh* [text|text]
+├ *${prefix}artinama* [barxnl]
+└ *${prefix}ramalhp* [082xxx]
+
+_*Others*_
+├ *${prefix}quran*
+├ *${prefix}nulis* [gada api]
+├ *${prefix}ocr*
+├ *${prefix}meme*
+├ *${prefix}memindo*
+├ *${prefix}testime*
+├ *${prefix}ttp*
+├ *${prefix}hobby*
+├ *${prefix}intro*
+├ *${prefix}bot*
+├ *${prefix}tomp3*
+├ *${prefix}watak*
+└ *${prefix}report*
+
+_*Download*_
+├ *${prefix}fb* [link]
+├ *${prefix}yt* [link] [fix]
+├ *${prefix}ytmp3* [link]
+├ *${prefix}ytmp4* [link]
+├ *${prefix}joox* [judul lagu] [fix]
+└ *${prefix}tiktok* [link] [Fix]
+
+_*Owner*_
+├ *${prefix}kudeta*
+├ *${prefix}bc*
+├ *${prefix}bc2*
+├ *${prefix}bcgc*
+├ *${prefix}leave*
+├ *${prefix}clearall*
+├ *${prefix}clone*
+├ *${prefix}block*
+├ *${prefix}unblock*
+├ *${prefix}hidetag*
+├ *${prefix}hidetag2*
+├ *${prefix}setprefix*
+├ *${prefix}bar*
+├ *${prefix}ban*
+├ *${prefix}getses*
+├ *${prefix}tag*
+╽
+╰╼ Ketik *${prefix}info* untuk melihat list informasi tentang bot
+       Ketik *${prefix}owner* untuk melihat kontak owner
+         Mau donasi? 081246460730(Telkom)
+         Jika tidak ingin donasi bantu Follow Ig aja ngab
+         _instagram.com/_sekhaa
+    _*FLINSKY_BOT © 2021*_`
+}
+exports.help = help
